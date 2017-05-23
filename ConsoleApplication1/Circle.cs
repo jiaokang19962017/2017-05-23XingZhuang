c131597fafec27b0c236ee5a27d6442d7e2619a9
jiaokang <Administrator@HP201-1>
2017-05-23 14:29:41 +0800

    继承实例

---
+++ ConsoleApplication1/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    /// <summary>
+    /// 派生类(子类):圆形
+    /// </summary>
+    class Circle:Shape
+    {
+        public Circle()
+        {
+
+        }
+
+        /// <summary>
+        /// 圆形构造函数
+        /// 初始化颜色值
+        /// </summary>
+        /// <param name="Color">颜色</param>
+        public Circle(string Color)
+        {
+            this.Color = Color;
+        }
+    }
+}
---
+++ ConsoleApplication1/Shape.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+
+    /// <summary>
+    /// 基类:形状类
+    /// </summary>
+    class Shape
+    {
+        /// <summary>
+        /// 颜色
+        /// </summary>
+       protected string Color;
+
+
+        /// <summary>
+        /// 形状的构造方法,初始化颜色值
+        /// </summary>
+        /// <param name="Color">颜色</param>
+        public Shape(string Color)
+        {
+            this.Color = Color;
+        }
+
+        /// <summary>
+        /// 无参的构造函数
+        /// </summary>
+        public Shape()
+        {
+
+        }
+
+        /// <summary>
+        /// 取颜色值
+        /// </summary>
+        /// <returns>返回颜色</returns>
+        protected string Getcolor()
+        {
+            return Color;
+        }
+    }
+}
